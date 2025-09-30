bba25a28bb98974b388eb7178ee0d2359bc99e6f
Sadman Noman Swopnil <sadmannomanswopnil@gmail.com>
2025-09-30 13:53:13 +1000

    Add score_program

---
+++ prac_02/score.py
@@ -0,0 +1,25 @@
+"""
+CP1404
+Fixed program to determine score status, with function
+"""
+
+
+def main():
+    """Get a numeric score and display its status."""
+    score = float(input("Enter score: "))
+    print(determine_status(score))
+
+
+def determine_status(score):
+    """Determine the status of a given score."""
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
