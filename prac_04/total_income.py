b31375ba43d3da4912c8c8439126fdd3bdb3240e
Sadman Noman Swopnil <sadmannomanswopnil@gmail.com>
2025-10-11 13:29:05 +1000

    Add total_income.py

---
+++ prac_04/total_income.py
@@ -0,0 +1,26 @@
+"""
+CP1404 Practical - Cumulative total income program
+"""
+
+
+def main():
+    """Display income report for incomes over a given number of months."""
+    incomes = []
+    number_of_months = int(input("How many months? "))
+
+    for month in range(1, number_of_months + 1):
+        income = float(input(f"Enter income for month {month}: "))
+        incomes.append(income)
+
+    print_report(incomes)
+
+
+def print_report(incomes):
+    """Print report based on incomes."""
+    print("\nIncome Report\n-------------")
+    total = 0
+    for month, income in enumerate(incomes, 1):
+        total += income
+        print(f"Month {month:2} - Income: ${income:10.2f} Total: ${total:10.2f}")
+
+main()
