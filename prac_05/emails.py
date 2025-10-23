72757c04bb995ee7eacbcc6e25e2b7e6ec366216
Sadman Noman Swopnil <sadmannomanswopnil@gmail.com>
2025-10-23 14:03:25 +1000

    Add emails.py

---
+++ prac_05/emails.py
@@ -0,0 +1,32 @@
+"""
+CP1404
+Email to name dictionary
+"""
+
+
+def main():
+
+    email_to_name = {}
+    email = input("Email: ")
+    while email != "":
+        name = extract_name_from_email(email)
+        confirmation = input(f"Is your name {name}? (Y/n) ")
+        # Anything other than Y or Enter is treated as a "no"
+        if confirmation.upper() != "Y" and confirmation != "":
+            name = input("Name: ")
+        email_to_name[email] = name
+        email = input("Email: ")
+
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
+
+
+def extract_name_from_email(email):
+    """Extract potential name from email address."""
+    prefix = email.split('@')[0]
+    parts = prefix.split('.')
+    name = " ".join(parts).title()
+    return name
+
+
+main()
